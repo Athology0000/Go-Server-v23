8791737508819f227b678f3dbaa864b21bd0b893
aeare <aearesluna@gmail.com>
2026-03-22 11:44:04 -0600

    feat: make WorldAccessor::inBuffer public for frontier A*

--- natives/src/engine/WorldAccessor.h
+++ natives/src/engine/WorldAccessor.h
@@ -13,6 +13,7 @@ public:
     bool isLadder(int x, int y, int z) const;
     bool isWater(int x, int y, int z) const;
     bool isLava(int x, int y, int z) const;
+    bool inBuffer(int x, int y, int z) const;
 
     const uint8_t* bufferData() const { return buffer_; }
     int originX() const { return bx_; }
@@ -23,7 +24,6 @@ private:
     const uint8_t* buffer_ = nullptr;
     int bx_ = 0, by_ = 0, bz_ = 0;
 
-    bool inBuffer(int x, int y, int z) const;
     uint8_t bufferAt(int x, int y, int z) const;
     uint8_t callbackBlock(int x, int y, int z) const;
 };
