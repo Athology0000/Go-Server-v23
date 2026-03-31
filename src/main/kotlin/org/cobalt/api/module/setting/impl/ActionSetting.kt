805c198bb0bd88397263ae996fdde19439d7aa29
aeare <aearesluna@gmail.com>
2026-03-31 00:25:26 -0600

    feat: add optional dynamic buttonLabelProvider to ActionSetting

--- src/main/kotlin/org/cobalt/api/module/setting/impl/ActionSetting.kt
+++ src/main/kotlin/org/cobalt/api/module/setting/impl/ActionSetting.kt
@@ -6,13 +6,20 @@ import org.cobalt.api.module.setting.Setting
 
 /**
  * Action button setting. Not persisted; used to trigger an action from the UI.
+ * If [buttonLabelProvider] is non-null, the button label is computed dynamically each frame.
  */
 class ActionSetting(
   name: String,
   description: String,
-  val buttonLabel: String,
-  private val onClick: () -> Unit,
+  staticLabel: String,
+  buttonLabelProvider: (() -> String)? = null,
+  onClick: () -> Unit,
 ) : Setting<Boolean>(name, description, false) {
+
+  private val _staticLabel = staticLabel
+  private val _buttonLabelProvider = buttonLabelProvider
+  private val _onClick = onClick
+  val buttonLabel: String get() = _buttonLabelProvider?.invoke() ?: _staticLabel
 
   override val defaultValue: Boolean = false
 
@@ -25,6 +32,6 @@ class ActionSetting(
   }
 
   fun trigger() {
-    onClick()
+    _onClick()
   }
 }
